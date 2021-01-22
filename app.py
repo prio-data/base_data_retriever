3bfcb86754f88737c4d83d20a5852d663ad70ee4
peder2911 <pglandsverk@gmail.com>
2021-01-22 14:51:27 +0100

    Generalized querying pattern, specified some stuff
    Code now works pretty well for pgm, cm.
    Need to add group / aggregate functionality to allow for
    aggregation-queries (e.g traversing the dep-graph in reverse).

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 """
 Internal API for getting data from the DB with a simple, clear interface.
 """
+from typing import Optional,List
+from datetime import date
 import fastapi
-
-#from uoa import uoa_list,uoa_post,uoa_detail
-#from retrieval import query,column
-#from decorators import parquet_bytes,csv_text
-from loa import variable_query,priogrid_month_query
+from pydantic import ValidationError
+from pydantic import BaseModel
+from fastapi import Response,Request
+import querying 
 
 app = fastapi.FastAPI()
 
-app.get("/{loa}/{var}")(variable_query)
-app.post("/priogrid_month/")(priogrid_month_query)
-
-"""
-app.get("/uoa/{name}")(uoa_detail)
-app.get("/uoa/")(uoa_list)
-app.post("/uoa/")(uoa_post)
-app.post("/data/{uoa}")(csv_text(query))
-app.get("/data/{uoa}/{column}")(csv_text(column))
-"""
+app.post("/{loa}/")(querying.json_query)
+#app.get("/{loa}/{var}/")
