9fbf3d564ebe8fec8bdc66fcfa52e91cdde6cca0
peder2911 <pglandsverk@gmail.com>
2021-03-10 16:40:21 +0100

    Hooked up with Azure appconfig

--- base_data_retriever/settings.py
+++ base_data_retriever/settings.py
@@ -1,8 +1,16 @@
 from environs import Env
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from azure.appconfiguration import AzureAppConfigurationClient
 
 env = Env()
 env.read_env()
 
-APP_CONFIG_CONNECTION_STRING = env.str("APP_CONFIG_CONNECTION_STRING")
-TIMECASTER_URL = env.str("TIMECASTER_URL")
-DB_SCHEMA = env.str("DB_SCHEMA")
+APP_CONFIG_CONNECTION_STRING = (SecretClient(env.str("KEY_VAULT_URL"),DefaultAzureCredential())
+    .get_secret("app-settings-connection-string")
+    .value
+    )
+
+app_config_client = AzureAppConfigurationClient.from_connection_string(APP_CONFIG_CONNECTION_STRING)
+
+DB_SCHEMA = app_config_client.get_configuration_setting("base-data-retriever-schema").value
