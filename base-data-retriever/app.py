de559a9cf329ce0b1eb1b80329e83118ab123fb8
peder2911 <pglandsverk@gmail.com>
2021-03-17 13:34:51 +0100

    No date filtering

--- base-data-retriever/app.py
+++ base-data-retriever/app.py
@@ -4,9 +4,6 @@ Internal API for getting data from the DB with a simple, clear interface.
 import io
 from contextlib import closing
 import fastapi
-import querying 
-import logging
-from datetime import date
 
 import pandas as pd
 
@@ -15,22 +12,15 @@ from db import Session
 from loa import index_columns
 from metadata import get_reflected_metadata
 from exceptions import QueryError,ConfigError
-from calls import cast_date_to_mid
 
 app = fastapi.FastAPI()
 
-#app.get("/{loa}/{var}/{year}/")(querying.variable_query)
-
-@app.get("/{loa}/{var}/{agg}/{year}/")
-def get_variable_value(loa: str, var: str, year: int, agg: str):
+@app.get("/{loa}/{var}/{agg}/")
+def get_variable_value(loa: str, var: str, agg: str):
     metadata = get_reflected_metadata()
 
     network = join_network(metadata.tables)
     table,variable = var.split(".")
-
-    month_table = [tbl for tbl in metadata.tables.values() if tbl.name == "month"][0]
-    start_month = cast_date_to_mid(date(year=year,month=1,day=1))
-    end_month = cast_date_to_mid(date(year=year,month=12,day=31))
 
     with closing(Session()) as sess:
         try:
@@ -40,11 +30,6 @@ def get_variable_value(loa: str, var: str, year: int, agg: str):
             return fastapi.Response(str(qe),status_code=400)
         except ConfigError as ce:
             return fastapi.Response(str(ce),status_code=500)
-
-        query = (query
-                .filter(month_table.c.id >= start_month)
-                .filter(month_table.c.id <= end_month)
-            )
 
         bytes_buffer = io.BytesIO()
 
