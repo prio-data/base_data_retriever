bf19d64820d7b0dd8e154d4d4f5990d64428dafa
peder2911 <pglandsverk@gmail.com>
2021-03-17 16:50:13 +0100

    Added test/prod settings

--- base-data-retriever/settings.py
+++ base-data-retriever/settings.py
@@ -1,30 +1,31 @@
+import os
 from environs import Env
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.appconfiguration import AzureAppConfigurationClient
+import requests
 
 env = Env()
 env.read_env()
 
-# = = = = = = = = = = = = = = = = = = = = =
-# SECRETS = = = = = = = = = = = = = = = = =
-# = = = = = = = = = = = = = = = = = = = = =
+PROD = env.bool("PRODUCTION","true")
 
-secret_client = SecretClient(env.str("KEY_VAULT_URL"),DefaultAzureCredential())
-get_secret = lambda k: secret_client.get_secret(k).value
+def get_dev_kv(k):
+    return requests.get(os.path.join(env.str("REST_ENV_URL",""),k)).content.decode()
+
+if PROD:
+    secret_client = SecretClient(env.str("KEY_VAULT_URL"),DefaultAzureCredential())
+    app_config_client = AzureAppConfigurationClient.from_connection_string(
+                get_secret("appconfig-connection-string")
+            )
+    get_secret = lambda k: secret_client.get_secret(k).value
+    get_config = lambda k: app_config_client.get_configuration_setting(k).value
+else:
+    get_secret = get_dev_kv
+    get_config = get_dev_kv
 
 DB_USER = get_secret("db-user")
 DB_PASSWORD = get_secret("db-password")
-
-# = = = = = = = = = = = = = = = = = = = = =
-# CONFIG= = = = = = = = = = = = = = = = = =
-# = = = = = = = = = = = = = = = = = = = = =
-
-app_config_client = AzureAppConfigurationClient.from_connection_string(
-            get_secret("app-settings-connection-string")
-        )
-
-get_config = lambda k: app_config_client.get_configuration_setting(k).value
 
 TIME_CASTER_URL = get_config("time-caster-url")
 DB_NAME = get_config("base-db-name")
