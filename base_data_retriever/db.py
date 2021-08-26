8f3ceda456fc184749406547c5b3d255a0dcd1ec
peder2911 <pglandsverk@gmail.com>
2021-08-26 12:05:17 +0200

    Rewrote database configuration
    Rewrote database config, moving from a custom implementation to a
    simpler sidecar-based solution. This makes DB config more reliable, as
    well as more flexible.

--- base_data_retriever/db.py
+++ base_data_retriever/db.py
@@ -1,17 +1,19 @@
-import os
+import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .settings import config
 
-connection_string = ("postgresql+psycopg2://"
-        f"{config('DB_USER')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+def get_con():
+    return psycopg2.connect(
+            f"host={config('DB_HOST')} "
+            f"port={config('DB_PORT')} "
+            f"user={config('DB_USER')} "
+            f"dbname={config('DB_NAME')} "
+            f"sslmode=allow "
         )
 
-if os.path.exists(os.path.expanduser("~/.postgresql")):
-    connection_string += "?sslmode=require"
-
-engine = create_engine(connection_string)
+engine = create_engine("postgresql+psycopg2://", creator = get_con)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
